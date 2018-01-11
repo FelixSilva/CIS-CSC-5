ab8ba1f195084d4f364ae94de4ad88553f1edfd8
FelixSilva <felixsilva109@outlook.com>
2018-01-11 14:04:06 -0800

    Signed-off-by: FelixSilva <felixsilva109@outlook.com>

---
+++ Lab/Lab011118/Truth_Table_1/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Felix Silva
+ * Created on January 11, 2018, 1:20 pm
+ * Purpose: Validate the Truth Table
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables 
+    
+    
+    // Initialize Variables 
+    
+    
+    //Process
+    
+    
+    //Output the Results 
+    
+  
+
+    //Exit
+    return 0;
+}
